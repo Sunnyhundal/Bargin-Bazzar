b57a6677bf7fa1011b88d56e3fb43ab702acc7a1
Rina Inada <indrn@icloud.com>
2023-08-19 22:10:31 +0000

    Add items queries

--- db/queries/items.js
+++ db/queries/items.js
@@ -1,5 +1,4 @@
 //favorites related database queries goes here
-
 const db = require("../connection");
 
 /**
@@ -14,10 +13,9 @@ const db = require("../connection");
  *          thumnnail_url: string} } a new item
  * @return {Promise<{}>} A promise to the item.
  */
-const addItem = (item) => {
-
+const addItem = function(item) {
   if (!item) {
-    console.error('Invalid item object');
+    console.error("Invalid item object");
     return Promise.resolve(null);
   }
 
@@ -29,27 +27,50 @@ const addItem = (item) => {
     item.created_at,
     item.seller_id,
     item.photo_url,
-    item.thumbnail_url];
+    item.thumbnail_url
+  ];
 
   const queryString = `INSERT INTO items (id, title, price, is_sold, created_at, seller_id, photo_url, thumbnail_url)
   VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
 
-  return db.query(queryString, queryParams)
-    .then((res) => {
+  return db
+    .query(queryString, queryParams)
+    .then(res => {
       return res.rows[0];
     })
-    .catch((error) => {
+    .catch(err => {
       return null;
     });
 };
 
 /**
+ * Get items from the database.
+ * @return {Promise<Array>} A promise the items
+ */
+const getAllItems = function() {
+  return db
+    .query(`SELECT * FROM items;`)
+    .then(res => res.rows)
+    .catch(err => {
+      console.error(err.message);
+      throw err;
+    });
+};
+
+/**
  * Filter item by price from the database.
- * @param {}
+ * @param {number} minPrice - The minimum price.
+ * @param {number} maxPrice - The maximum price.
  * @return {Promise<{}>} A promise to the user.
  */
+const filterItemByPrice = function(minPrice, maxPrice) {
+  const queryString = `SELECT * FROM items WHERE price >= $1 AND price <= $2;`;
 
-const filterItemByPrice = (minPrice, maxPrice) => {
-  const queryString = ` SELECT * FROM items
-  WHERE price >= $1 AND price <= $2;`;
-}
+  return db
+    .query(queryString)
+    .then((res) => res.rows)
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
+};
