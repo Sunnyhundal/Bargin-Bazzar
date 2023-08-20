7e474ab1b25bd7f678865bc8a00513ff3a04759d
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-08-20 15:21:00 -0700

    create createFavorite and unFavorite routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,4 +1,4 @@
-const { getFavoritesByUserId } = require('../db/queries/favorites');
+const { getFavoritesByUserId, createFavorite, unFavorite } = require('../db/queries/favorites');
 const express = require("express");
 const router = express.Router();
 
@@ -22,6 +22,40 @@ router.get('/:userId', async (req, res) => {
   res
     .status(200)
     .json(response.favorites);
+});
+
+router.post('/', async (req, res) => {
+  const userId = req.session.userId;
+  const itemId = req.body.itemId;
+  const response = await createFavorite(userId, itemId);
+
+  if (response.error) {
+    res
+      .status(500)
+      .json({ error: 'Cannot create favorite' });
+
+    return;
+  }
+
+  res
+    .status(200);
+});
+
+router.delete('/:favoriteId', async (req, res) => {
+  const userId = req.session.userId;
+  const favoriteId = req.params.favoriteId;
+  const response = await unFavorite(favoriteId, userId);
+
+  if (response.error) {
+    res
+      .status(500)
+      .json({ error: 'Cannot delete favorite' });
+
+    return;
+  }
+
+  res
+    .status(200);
 });
 
 module.exports = router;
