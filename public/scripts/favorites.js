77178d452eb68d34b8b5a533106230663272c48a
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-08-20 15:03:24 -0700

    create favorites template

---
+++ public/scripts/favorites.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-undef */
+
+/**
+ * @typedef {import('../../db/queries/favorites.js').Favorite} Favorite
+ */
+
+const FAVORITES_PER_ROW = 4;
+
+/**
+ * @param {number} price - price is in cents
+ * @return {string} currency formatted string of the price
+ */
+const formatCurrency = (price) => {
+  return new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD',
+    minimumFractionDigits: 2
+  }).format(price / 100);
+};
+
+/**
+ * @param {Favorite} favorite - favorite is an object
+ * @return {string} HTML string of the favorite
+ */
+const createItem = (favorite) => `
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${favorite.title}</h5>
+      <span>${formatCurrency(favorite.price)}</span>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+`;
+
+/**
+ * Creates a row of favorites. Each row contains 4 favorites items.
+ *
+ * @param {any} _ - unused
+ * @param {number} index - index is the index of the favorites
+ * @returns {jQuery} jQuery object of the row
+ */
+const createRow = (_, index) => {
+  const rowId = Math.floor(index / FAVORITES_PER_ROW);
+  return $(`<ul class="favorites-row" id="favorites-row-${rowId}"}></ul>`);
+};
+
+/**
+ * @param {Favorite[]} favorites - favorites is an array of favorite objects
+ * @return {void}
+ */
+const renderFavorites = (favorites) => {
+  $('.favorites-container').empty();
+
+  const rowSize = Math.ceil(favorites.length / FAVORITES_PER_ROW);
+  // rows for favorites. each row should contain 4 favorite items based on design spec
+  const rows = Array.from({ length: rowSize}, createRow);
+
+  // append favorites to each row to be 4 items per row
+  rows.forEach(($row, index) => {
+    const startIndex = index * FAVORITES_PER_ROW;
+    const endIndex = (index + 1) * FAVORITES_PER_ROW;
+    const slicedFavorites = favorites.slice(startIndex, endIndex);
+
+    slicedFavorites.forEach((favorite) => {
+      $row.append(createItem(favorite));
+    });
+
+    $('.favorites-container').append($row);
+  });
+};
+
+$(document).ready(() => {
+  $.ajax({
+    method: 'GET',
+    url: '/favorites/6' // userId is hardcoded for now
+  })
+    .then((favorites) => {
+      renderFavorites(favorites);
+    });
+});
