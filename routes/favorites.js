48865d11fbc1942136513377dd6c2b857cb88499
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-08-20 15:01:47 -0700

    update favorites routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,27 @@
-// favorites-related routes
-
+const { getFavoritesByUserId } = require('../db/queries/favorites');
 const express = require("express");
 const router = express.Router();
 
+// favorites-related routes
 router.get("/", (req, res) => {
   res.render("favorites");
+});
+
+router.get('/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const response = await getFavoritesByUserId(userId);
+
+  if (response.error) {
+    res
+      .status(500)
+      .json({ error: 'Cannot get favorites' });
+
+    return;
+  }
+
+  res
+    .status(200)
+    .json(response.favorites);
 });
 
 module.exports = router;
