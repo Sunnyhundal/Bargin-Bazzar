aa4d049382411a3d77a06cee583d2d36ae2ba0f3
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-08-20 15:00:52 -0700

    create getFavoritesByUserId for favorites

--- db/queries/favorites.js
+++ db/queries/favorites.js
@@ -1,18 +1,37 @@
-//favorites related database queries goes here
-
 const db = require('../connection');
 
 /**
- * Only shows favorites that belong to the logged-in user from urlDatabase
- * @param {string} id - the id of the currently logged-in user
- * @Returns the favorites where the userID is equal to
+ * @typedef {Object} Favorite
+ * @property {string} title - the title of the item
+ * @property {number} id - the id of the favorite
+ * @property {number} price - the price of the item in cents
+ * @property {number} item_id - the id of the item
  */
-function favoritesForUser(id, favoritesDB) {
-  const userfavorites = {};
-  for (const favorite in favoritesDB) {
-    if (favoritesDB[favorite].userID === id) {
-      userfavorites[favorite] = favoritesDB[favorite];
-    }
-  }
-  return userfavorites;
-}
+
+/**
+ * Get favorites from the database by user id.
+ *
+ * @param {number} userId - the id of the user
+ * @return {Promise<{ favorites?: Favorite[], error?: any }>} A promise to the favorites.
+ */
+const getFavoritesByUserId = (userId) => {
+  const query = `
+    SELECT
+      f.id AS id,
+      i.title AS title,
+      i.price AS price,
+      i.id AS item_id
+    FROM favorites f
+    JOIN items i
+      ON i.id = f.item_id
+    WHERE user_id = $1;
+  `;
+
+  return db.query(query, [userId])
+    .then((response) => ({ favorites: response.rows}))
+    .catch((error) => ({ error }));
+};
+
+module.exports = {
+  getFavoritesByUserId
+};
