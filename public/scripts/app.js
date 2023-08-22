c5f2b10335e0b490b6434ee8344921cb9c9b9c28
sarahmanhard <smanhard88@gmail.com>
2023-08-21 17:39:27 -0700

    add search_bar.ejs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,1 @@
-const express = require('express')
-const app = express()
-const port = 8080
 
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-    res.render('pages/index')
-})
-app.listen(port, () => {
-  console.log(`App listening at port ${port}`)
-})
