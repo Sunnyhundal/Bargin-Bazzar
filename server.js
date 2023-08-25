6ff8d3584482a2f133f5ee0695fcbbf74a1a649d
Rina Inada <indrn@icloud.com>
2023-08-25 00:08:12 +0000

    Test dropdown frontend

--- server.js
+++ server.js
@@ -64,3 +64,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+//test
+app.get('/dropdown', (req, res) => {
+  res.render('dropdown');
+});
