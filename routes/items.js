de71a6daefe07cf9e4cea6d77b4864bf4f711677
sarahmanhard <smanhard88@gmail.com>
2023-08-24 18:45:06 -0700

    fix error where edit item not displaying

--- routes/items.js
+++ routes/items.js
@@ -24,4 +24,35 @@ router.get('/mylisting', (req, res) => {
   res.render('my-listing');
 });
 
+router.get('/:itemId/edit', async (req, res) => {
+  const itemId = req.params.itemId;
+
+  try {
+    const item = await itemDB.getItemById(itemId);
+    // Render the edit form view with the item's data
+    res.render('edit-item', { item });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('An error occurred');
+  }
+});
+
+router.post('/:itemId/edit', async (req, res) => {
+  const itemId = req.params.itemId;
+  const updatedData = req.body; // Assuming the form data is properly structured
+
+  try {
+    const item = await itemDB.getItemById(itemId);
+    if (!item) {
+      res.status(404).send('Item not found');
+      return;
+    }
+    // Render the edit form view with the item's data
+    res.render('edit-item', { item });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error fetching item data');
+  }
+  });
+
 module.exports = router;
