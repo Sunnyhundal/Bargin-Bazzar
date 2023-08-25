fbf462ddc7e4a60528077f2443ab43872683b9e9
Rina Inada <indrn@icloud.com>
2023-08-25 06:52:40 +0000

    Implement sort items

--- routes/items-api.js
+++ routes/items-api.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 const itemDB = require("../db/queries/items");
 const usersDB = require("../db/queries/users");
+const db = require("../db/connection");
 
 //this routes start with /api/items
 
 // Read: display all items
 router.get("/", (req, res) => {
+  const userId = req.cookies.userId;
 
   itemDB
     .getAllItems()
     .then((items) => {
-      res.render("items-list", { items });
+      res.render("index2", { items, userId });
 
     })
     .catch((err) => {
@@ -39,6 +41,7 @@ router.post("/new", (req, res) => {
 router.get("/:itemId", async (req, res) => {
   try {
     const itemId = req.params.itemId;
+    const userId = req.cookies.userId;
 
     const item = await itemDB.getItemById(itemId)
     let itemArray = Object.values(item);
@@ -52,7 +55,7 @@ router.get("/:itemId", async (req, res) => {
     const sellerID = item.seller_id;
     const sellerInfo = await usersDB.getUserInfo(sellerID);
 
-    res.render("item", { item, userEmail, sellerInfo });
+    res.render("item", { item, userEmail, sellerInfo, userId });
   } catch (err) {
     console.error(err.message);
     res.send("error occured");
@@ -83,5 +86,50 @@ router.delete("/:itemId/delete", (req, res) => {
       res.status(500).json({ error: "An error occured" });
     });
 });
+
+router.get("/mylisting/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  itemDB
+    .getItemsByUserId(userId)
+    .then((items) => {
+      res.json(items);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "An error occured" });
+    });
+});
+
+router.post('/filter', (req, res) => {
+  const choice = req.body.choice;
+  const userId = req.cookies.userId;
+
+  // Perform database query based on choice
+  let query;
+  if (choice === 'high-to-low') {
+    query = 'SELECT * FROM items ORDER BY price DESC';
+  } else if (choice === 'low-to-high') {
+    query = 'SELECT * FROM items ORDER BY price ASC';
+  } else if (choice === 'alphabetical') {
+    query = 'SELECT * FROM items ORDER BY title ASC';
+  } else {
+    // Handle other choices as needed
+    query = 'SELECT * FROM items';
+  }
+
+  db.query(query)
+    .then(result => {
+      const items = result.rows;
+      // res.json(rows);
+      res.render('index2', { items, userId })
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+
+
 
 module.exports = router;
